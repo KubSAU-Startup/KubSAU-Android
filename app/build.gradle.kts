48192ad698d444a6aaf87d34725507186dae0fc7
Danil Nikolaev <lischenkodev@gmail.com>
2024-06-28 16:23:25 +0300

    remove unused tests, dependencies and enabled onbackinvoke gesture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         release {
             signingConfig = signingConfigs.getByName("release")
 
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
 
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -81,23 +81,16 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
-    implementation("androidx.compose.foundation:foundation-layout:1.7.0-beta03")
+    implementation("androidx.compose.foundation:foundation-layout:1.7.0-beta04")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.camera:camera-core:1.3.4")
-    implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
@@ -108,9 +101,11 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
+
     implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
     implementation("com.squareup.moshi:moshi:1.15.1")
     ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
+
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
@@ -128,10 +123,9 @@ dependencies {
     implementation("androidx.camera:camera-extensions:$cameraxVersion")
     implementation("androidx.camera:camera-mlkit-vision:$cameraxVersion")
     implementation("com.google.mlkit:barcode-scanning:17.2.0")
+    implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
 
     implementation("com.google.accompanist:accompanist-permissions:0.35.1-alpha")
-
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     implementation("androidx.preference:preference-ktx:1.2.1")
 
